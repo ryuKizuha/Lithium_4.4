04d1b9b99efa75c88837df587e3c37d01f9cee6f
root <zulfiray7@gmail.com>
2021-01-24 22:41:23 +0900

    push

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export KERNELNAME=Lithium
+
+export LOCALVERSION=typeD
+
+export KBUILD_BUILD_USER=zRyu
+
+export KBUILD_BUILD_HOST=#FunProject
+
+export TOOLCHAIN=gcc
+
+export DEVICES=lavender
+
+source helper
+
+gen_toolchain
+
+send_msg "⏳ lagi build kernelnya ${KERNELNAME} ${LOCALVERSION} for ${DEVICES}..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do 
+
+	build ${i} -oldcam
+
+        build ${i} -newcam
+
+done
+
+END=$(date +"%s")
+ 
+DIFF=$(( END - START ))
